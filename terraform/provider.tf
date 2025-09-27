d81670d95f9853b199fe78e326e456c5e50a6fcf
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-09-27 18:57:28 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,3 +8,11 @@ terraform {
     }
   }
 }
+provider "azurerm" {
+  features {}
+  # Uses Azure DevOps Service Connection
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
