be91b58f07bf37cb3f632fc41c1b540b70781b84
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-09-27 18:58:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,9 @@
 # Terraform configuration file
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Environment = var.environment
+    Project     = "Terraform-AzurePipeline-Demo"
+  }
+}
