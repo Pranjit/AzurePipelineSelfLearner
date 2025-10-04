4e41bd0a53299906b4e11e3bc5f9c38ced1143ad
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-10-04 22:29:50 +0530

    Update main.tf

--- terraform/azure-linux-vmss-static-site/main.tf
+++ terraform/azure-linux-vmss-static-site/main.tf
@@ -1,1 +1,112 @@
-main.tf
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vmss-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "vmss-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "vmss-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "vmss-lb"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "PublicIP"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bepool" {
+  name                = "BackendPool"
+  loadbalancer_id     = azurerm_lb.lb.id
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_lb_probe" "http" {
+  name                = "http-probe"
+  resource_group_name = var.resource_group_name
+  loadbalancer_id     = azurerm_lb.lb.id
+  protocol            = "Http"
+  port                = 80
+  request_path        = "/"
+}
+
+resource "azurerm_lb_rule" "http" {
+  name                           = "http-rule"
+  resource_group_name            = var.resource_group_name
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "PublicIP"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bepool.id
+  probe_id                       = azurerm_lb_probe.http.id
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+  name                            = "web-vmss"
+  location                        = var.location
+  resource_group_name             = var.resource_group_name
+  sku                             = "Standard_B1s"
+  instances                       = 2
+  admin_username                  = "azureuser"
+  admin_password                  = "P@ssword1234!"
+  disable_password_authentication = false
+  upgrade_mode                    = "Manual"
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  network_interface {
+    name    = "vmss-nic"
+    primary = true
+
+    ip_configuration {
+      name                                   = "internal"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.subnet.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bepool.id]
+    }
+  }
+
+  extension {
+    name                 = "install-nginx-html"
+    publisher            = "Microsoft.Azure.Extensions"
+    type                 = "CustomScript"
+    type_handler_version = "2.0"
+
+    settings = jsonencode({
+      "commandToExecute" = "apt update && apt install -y nginx git && git clone ${var.html_repo_url} /var/www/html/"
+    })
+  }
+}
