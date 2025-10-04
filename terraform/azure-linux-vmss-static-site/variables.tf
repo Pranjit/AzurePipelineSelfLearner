49cceac0e46a334e1f7e43d1138c2d3a802560f3
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-10-04 22:29:09 +0530

    Update variables.tf

--- terraform/azure-linux-vmss-static-site/variables.tf
+++ terraform/azure-linux-vmss-static-site/variables.tf
@@ -1,1 +1,15 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "html_repo_url" {
+  description = "GitHub repo with static HTML files"
+  type        = string
+}
