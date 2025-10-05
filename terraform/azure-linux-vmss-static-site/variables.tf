330db3ce7d17abc21f6239d1ce864975123f7d38
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-10-05 21:05:09 +0530

    Update variables.tf

--- terraform/azure-linux-vmss-static-site/variables.tf
+++ terraform/azure-linux-vmss-static-site/variables.tf
@@ -13,3 +13,22 @@ variable "html_repo_url" {
   description = "GitHub repo with static HTML files"
   type        = string
 }
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure service principal client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure service principal client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
