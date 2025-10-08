fbb7e3401acdc295d71db82e8f515cb82abbceec
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-10-08 17:21:17 +0530

    Update main.tf

--- terraform/azure-linux-vmss-static-site/main.tf
+++ terraform/azure-linux-vmss-static-site/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg" {
-  name     = ${var.resource_group_name}-${var.commit_id}
+  name     = "${var.resource_group_name}-${var.commit_id}"
   location = var.location
 }
 
