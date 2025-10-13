036b8f334af7ca78d9634dce403eb103a59feb52
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-10-14 00:48:10 +0530

    Update variables.tf

--- terraform/azure-appservice/variables.tf
+++ terraform/azure-appservice/variables.tf
@@ -1,7 +1,26 @@
 variable "location" {
   default = "East US"
 }
-
 variable "app_service_plan_sku" {
   default = "B1"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure service principal client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure service principal client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
 }
