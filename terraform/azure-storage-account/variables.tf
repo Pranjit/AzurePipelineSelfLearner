dbb6d009ee740c719e462cf2f4e456153d7f10f9
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-10-19 22:48:44 +0530

    Update variables.tf

--- terraform/azure-storage-account/variables.tf
+++ terraform/azure-storage-account/variables.tf
@@ -1,1 +1,38 @@
+# Input variables
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
 
+variable "client_id" {
+  type        = string
+  description = "Azure service principal client ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure service principal client secret"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+  default     = "rg-demo-terraform"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "East US"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment tag"
+  default     = "dev"
+}
