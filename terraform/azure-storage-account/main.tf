01cdb91e8889d574912842517328ffe03d79e42c
Pranjit Kalita <kalita.pranjit@hotmail.com>
2025-10-19 22:52:31 +0530

    Update main.tf

--- terraform/azure-storage-account/main.tf
+++ terraform/azure-storage-account/main.tf
@@ -7,14 +7,12 @@ resource "azurerm_resource_group" "rg" {
 # Storage Account
 # ------------------------------------------------------------
 resource "azurerm_storage_account" "storage" {
-  name                     = "mystorageacctdemo123"   # must be globally unique, lowercase, 3–24 chars
+  name                     = "mystorageacctdemo123"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"               # Standard or Premium
-  account_replication_type = "LRS"                    # LRS, GRS, RAGRS, ZRS
-
-  # Optional: enable public access or features
-  allow_blob_public_access = false
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 
   tags = {
     environment = "demo"
